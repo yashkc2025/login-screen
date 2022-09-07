955a42af105778d8b9803019cb57d461a49ce220
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-09-07 18:41:28 +0530

    Barebone Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:simple_gradient_text/simple_gradient_text.dart';
-import 'package:hexcolor/hexcolor.dart';
 void main(){
   runApp(const MLogin());
 }
@@ -13,17 +11,39 @@ class MLogin extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.white,
+          // backgroundColor: Color(0xff34344A),
           body: SafeArea(
             child : Column(
               children: [
                 const SizedBox(height: 120,),
-                Center(child: const Text('Login',style: TextStyle(fontSize: 80,fontWeight: FontWeight.w100))),
-                Container(
-                  height: 50,
-                  width: 300,
-                  color: Colors.deepOrange,
-                  child : Text('Login', style: TextStyle(fontSize: 25,color: Colors.white),)
+                // const Center(child: Text('Login',style: TextStyle(fontSize: 80,fontWeight: FontWeight.w100))),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),fillColor : Colors.white60,filled:true,hintText: "Email",prefixIcon: Icon(Icons.alternate_email_sharp, color: Color(0xff3234FB),),focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(30),borderSide: BorderSide(color: Colors.pinkAccent,width:1)))),
+                ),
+                  // SizedBox(height: 20,),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                    child: TextFormField(decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),fillColor : Colors.white60,filled:true,hintText: "Password",suffixIcon: Icon(Icons.visibility_off),prefixIcon: Icon(Icons.lock, color: Color(0xff3234FB),),focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(30),borderSide: BorderSide(color: Colors.pinkAccent,width:1))))),
+
+                SizedBox(height: 60,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 250,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Color(0xfff0386b),),
+                      child : const Center(child : Text('Log In', style: TextStyle(fontSize: 25,color: Colors.white))),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      height: 60,
+                      width: 100,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Color(0xffF4F1BB),),
+                      child : const Center(child : Text('Sign Up', style: TextStyle(fontSize: 20,color: Colors.black))),
+                    ),
+                  ],
                 )
               ],
             ),
